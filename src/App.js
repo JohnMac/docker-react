68209ea276984b6f9ba1d408138bb5d2e8653adc
Rafed Razooki <rraz@netcompany.com>
2021-03-05 14:30:36 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hi there fella how's it going
         </p>
         <a
           className="App-link"
